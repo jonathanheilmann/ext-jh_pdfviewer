e7ee0e3a811955c5ac7efb26df64943558a9724a
jonathanheilmann <mail@jonathan-heilmann.de>
2015-06-03 20:22:40 +0200

    [ENHANCEMENT] Update ext_emconf and set version to 0.0.3-dev (Resolves #2)

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,37 @@
 <?php
 
 ########################################################################
+<?php
+
+/***************************************************************
 # Extension Manager/Repository config file for ext "jh_pdfviewer".
-#
+ *
 # Auto generated 03-05-2015 17:06
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+ *
+ * Manual updates:
+ * Only the data in the array - anything else is removed by next write.
+ * "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'PDF viewer',
 	'description' => 'Display a pdf as image(s) at your website',
-	'category' => 'services',
-	'shy' => 1,
-	'version' => '0.0.2',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'TYPO3_version' => '6.0.0-6.2.99',
-	'PHP_version' => '',
-	'module' => '',
-	'state' => 'beta',
-	'uploadfolder' => 1,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 1,
-	'lockType' => '',
+	'category' => 'plugin',
 	'author' => 'Jonathan Heilmann',
 	'author_email' => 'mail@jonathan-heilmann.de',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '1',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 1,
+	'version' => '0.0.3-dev',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
 );
-
-?>
